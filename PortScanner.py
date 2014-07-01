3280aad25e7bccc944ef931723bce0063c19acf7
John Freed <jtf323@gmail.com>
2014-07-01 14:53:53 -0400

    Minor cleanup

--- PortScanner.py
+++ PortScanner.py
@@ -3,34 +3,30 @@ import sys
 
 
 def connect_to_ip(ip, port):
-	
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((ip, port))
-       
-	return sock
+        return sock
 
-    except:
-    	return None
+    except Exception:
+        return None
 
 
 def scan_port(ip, port, timeout):
-
     socket.setdefaulttimeout(timeout)
     sock = connect_to_ip(ip, port)
 
     if sock:
-        print("Able to connect to: %s:%d"%(ip, port))
-        sock.close() 
+        print("Able to connect to: %s:%d" % (ip, port))
+        sock.close()
     else:
-        print("Not able to connect to: %s:%d"%(ip, port))
-
+        print("Not able to connect to: %s:%d" % (ip, port))
 
 
 # Get user input
 ip_domain = raw_input("Enter the ip or domain: ")
-port = raw_input("Enter the port range: ")
-timeout = raw_input("Timeout [Default=5]: ")
+port = raw_input("Enter the port range (Ex 20-80): ")
+timeout = raw_input("Timeout (Default=5): ")
 if not timeout:
     timeout = 5
 
@@ -39,7 +35,7 @@ port_range = port.split("-")
 # Get the IP address if the host name is a domain
 try:
     ip = socket.gethostbyname(ip_domain)
-except:
+except Exception:
     print 'There was an error resolving the domain'
     sys.exit(1)
 

ae764894618e16399390004934e848586ea1a095
John Freed <jtf323@gmail.com>
2014-07-01 14:00:09 -0400

    Minor fixes / changes
    Allow user to specify timeout, set the timeout before trying to connect,
    added error handling if the domain couldn't be resolved

--- PortScanner.py
+++ PortScanner.py
@@ -1,40 +1,47 @@
 import socket
+import sys
 
 
 def connect_to_ip(ip, port):
 	
     try:
-    
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.connect((ip, port))
+        sock.connect((ip, port))
        
 	return sock
 
     except:
-
     	return None
 
-def scan_ports(ip, port):
 
+def scan_port(ip, port, timeout):
+
+    socket.setdefaulttimeout(timeout)
     sock = connect_to_ip(ip, port)
-    socket.setdefaulttimeout(5)
 
     if sock:
-        
-	print("Able to connect to: %s:%d"%(ip, port))
-	sock.close() 
-
+        print("Able to connect to: %s:%d"%(ip, port))
+        sock.close() 
     else:
-
         print("Not able to connect to: %s:%d"%(ip, port))
-        
-        
+
+
+
+# Get user input
 ip_domain = raw_input("Enter the ip or domain: ")
 port = raw_input("Enter the port range: ")
+timeout = raw_input("Timeout [Default=5]: ")
+if not timeout:
+    timeout = 5
+
 port_range = port.split("-")
 
-ip=socket.gethostbyname(ip_domain) #get ip if the host name is a domain
+# Get the IP address if the host name is a domain
+try:
+    ip = socket.gethostbyname(ip_domain)
+except:
+    print 'There was an error resolving the domain'
+    sys.exit(1)
 
-for i in range(int(port_range[0]), int(port_range[1])):
-    scan_ports(ip, int(i))
-
+for port in range(int(port_range[0]), int(port_range[1])):
+    scan_port(ip, int(port), int(timeout))
